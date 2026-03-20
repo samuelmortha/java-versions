29e84ee6626de9cca54b9efe4ef1e838d9bccfe5
EC2 Default User <ec2-user@ip-172-31-28-81.eu-west-2.compute.internal>
2026-03-20 23:57:41 +0000

    add python code

---
+++ python/GettingStarted1/welcometopython.py
@@ -0,0 +1,14 @@
+# Comment begining with #
+print('Welcome to python programming')
+x = 5 # end of line comment
+
+if x > 4 :
+    print('X > 4 is print')
+if x > 10 :
+    print('x > 10')
+    
+"""
+This is a 
+multi line
+comment
+"""
